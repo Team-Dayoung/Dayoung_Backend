913c79492f52f2498af6faeba88ae40ba592a89c
Tyler <tylereunholee010203@gmail.com>
2024-11-08 11:32:18 +0900

    feat :: [#1] UserMajors 엔티티 개발
    사용자 전공 엔티티

--- src/user-majors/user-majors.module.ts
+++ src/user-majors/user-majors.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserMajorsService } from './user-majors.service';
 import { UserMajorsController } from './user-majors.controller';
+import { UserMajorsModel } from './entity/user-majors.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserMajorsModel])],
   controllers: [UserMajorsController],
   providers: [UserMajorsService],
 })
