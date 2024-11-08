0d336295af8bc44c358cfe47054e860e1205fdb4
Tyler <tylereunholee010203@gmail.com>
2024-11-08 11:34:54 +0900

    chore :: [#1] 불필요한 모듈 삭제
    사용자 기술스택 모듈은 필요없다고 판단되어 삭제합니다.

--- src/user-skills/user-skills.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UserSkillsService } from './user-skills.service';
-
-@Controller('user-skills')
-export class UserSkillsController {
-  constructor(private readonly userSkillsService: UserSkillsService) {}
-}
